47b91805afeb3c537e894c20d30297a8e5a59469
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-03-23 20:23:29 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cadastro-filho';
+  public nome:string = '';
+  public datanascimento:string = '';
+
+  public borda_nome:string = '';
+  public borda_datanascimento:string = '';
+
+  salvar():boolean {
+    if (this.nome == ''){
+      this.borda_nome = 'border:1px solid #FF0000';
+      return false;
+    }
+    if (this.datanascimento == ''){
+      this.borda_datanascimento = 'border:1px solid #FF0000';
+      return false;
+    }
+
+    let dt = this.datanascimento
+    .replace('/','').replace('/','');
+
+    if (dt.length != 8 || !this.isValidDate(this.datanascimento)){
+      this.borda_datanascimento = 'border:1px solid #FF0000';
+      return false;     
+    }
+
+    return true;
+  }
+
+  isValidDate(data:string):boolean{
+    let partes = data.split('/');
+    let dia:number = parseInt(partes[0]);
+    let mes:number = parseInt(partes[1]);
+    let ano:number = parseInt(partes[2]);
+
+    if (dia < 1 || dia > 31){
+      return false;
+    }
+    if (mes < 1 || mes > 12){
+      return false;
+    }
+    if (mes == 2 && dia > 29){
+      return false;
+    }
+    return true;
+  }
 }
