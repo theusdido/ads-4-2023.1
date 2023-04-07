a8f57f8e437e2330a6489d1291995fb2b4e297c6
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-04-06 21:22:49 -0300

    Correção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-cidade-estado';
+  public estados:Array<any> = [
+    {sigla:'SC' , nome:'Santa Catarina'},
+    {sigla:'PR' , nome:'Paraná'},
+    {sigla:'RS' , nome:'Rio Grande do Sul'},
+  ];
+
+  public cidades:Array<any> = [
+    {id:1 , nome:'Criciúma' , estado: 'SC'},
+    {id:2 , nome:'Porto Alegre' , estado: 'RS'},
+    {id:3 , nome:'Curitiba' , estado: 'PR'},
+    {id:4 , nome:'Florianópolis' , estado: 'SC'},
+    {id:5 , nome:'Flores da Cunha' , estado: 'RS'},
+    {id:6 , nome:'Içara' , estado: 'SC'},
+    {id:7 , nome:'Criciúma' , estado: 'PR'}
+  ];
+  
+  public estado:string = 'SC';
 }
