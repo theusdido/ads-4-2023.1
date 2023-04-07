48a398a97923286f5905feed183f0496ea62c75d
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-04-06 21:21:33 -0300

    Correção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recibo-vendas';
+  
+  public cliente:string = '';
+  public produto:string = '';  
+  public quantidade:number = 1;
+  public valor:number = 0;
+
+  public display_recibo:boolean = false;
+  public display_form:boolean = true;
+
+  inc(){
+    this.quantidade++;
+  }
+
+  dec(){
+    if (this.quantidade > 1){
+      this.quantidade--;
+    }
+  }
+
+  showRecibo(){
+    this.display_recibo = true;
+    this.display_form = false;
+  }
+  showForm(){
+    this.display_recibo = false;
+    this.display_form = true;    
+  }
+  valorTotal():number{
+    return this.quantidade * this.valor;
+  }
 }
