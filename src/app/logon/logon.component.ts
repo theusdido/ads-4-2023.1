39689ac93588f4b7a3ea45d59468cfc518c7aba1
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-04-25 20:24:04 -0300

    Tela de Autenticação

--- src/app/logon/logon.component.ts
+++ src/app/logon/logon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logon',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./logon.component.css']
 })
 export class LogonComponent {
+  public email:string = '';
+  public senha:string = '';
 
+  constructor(
+    public router:Router
+  ){  
+  }
+  autenticar(){
+    let usuario_ls = JSON.parse(String(localStorage.getItem('usuario')));
+    let email_ls = usuario_ls.email;
+    let senha_ls = atob(usuario_ls.senha);
+
+    if (email_ls == this.email && senha_ls == this.senha){
+      // Permitir Acesso ...
+      this.router.navigateByUrl('/home');
+    }else{
+      alert('Não Permitido');
+      // Negar Acesso ...
+    }
+  }
 }
