93f5dde63cbff7f025d17bde7a00cf4587868895
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-05-25 20:58:52 -0300

    Filtro com listagem de produtos

--- src/app/filtro-produto/filtro-produto.component.ts
+++ src/app/filtro-produto/filtro-produto.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { ProdutoService } from '../service/produto.service';
 
+declare var bootstrap:any;
+
 @Component({
   selector: 'app-filtro-produto',
   templateUrl: './filtro-produto.component.html',
@@ -13,15 +15,25 @@ export class FiltroProdutoComponent {
   public nome_produto:string = '';
   constructor(
     public produto_service:ProdutoService
-  ){}
+  ){
+    this.produto_service.carregar();
+  }
 
   carregar(){
     let produto = this.produto_service.registro(Number(this.codigo_produto));
-    if (produto != undefined){
+    if (produto != undefined && this.codigo_produto != ''){
       this.nome_produto   = produto.nome;
     }else{
       this.codigo_produto = '';
       this.nome_produto = '';
     }
+  }
+
+  setProduto(indice_produto:number){
+    this.codigo_produto = String(indice_produto);
+    this.carregar();   
+
+    let modal = bootstrap.Modal.getInstance(document.getElementById('lista-pesquisa-produto'));
+    modal.hide();
   }
 }
